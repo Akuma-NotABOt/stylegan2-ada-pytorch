632d2ac86ae9fd460b17a2665e164b438a667b31
Richard Kuo <rkuo2000@gmail.com>
2021-09-15 16:40:04 +0800

    Update align_images.py

--- align_images.py
+++ align_images.py
@@ -4,8 +4,8 @@ import os
 import sys
 import bz2
 from tensorflow.keras.utils import get_file
-from ffhq_dataset.face_alignment import image_align
-from ffhq_dataset.landmarks_detector import LandmarksDetector
+from face_alignment import image_align
+from landmarks_detector import LandmarksDetector
 
 LANDMARKS_MODEL_URL = 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2'
 
